b9a686c8914fed6f15a4c793ef6b2f4b08fd658e
Djamal <saktybeev@yandex.ru>
2024-10-20 16:56:35 +0300

    Update

--- module_14_1.py
+++ module_14_1.py
@@ -15,5 +15,16 @@ for i in range(1, 11):
     cur.execute(" INSERT INTO Users(username, email, age, balance) VALUES (?,?,?,?)",
                 (f"User{i}", f"example{i}@gmail.com", str(i * 10), "1000"))
 
+for i in range(1, 11, 2):
+    cur.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"User{i}"))
+
+for i in range(1, 11, 3):
+    cur.execute("DELETE FROM Users WHERE username = ?", (f"User{i}",))
+
+cur.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+users = cur.fetchall()
+for user in users:
+    print(user)
+
 connection.commit()
 connection.close()
